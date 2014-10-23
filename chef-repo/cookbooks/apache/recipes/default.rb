c09a45a23de664b4a6515d133af64b2fbe7db35b
nathenharvey <nharvey@getchef.com>
2014-10-23 06:39:12 -0400

    a basic apache recipe

--- chef-repo/cookbooks/apache/recipes/default.rb
+++ chef-repo/cookbooks/apache/recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright (c) 2014 The Authors, All Rights Reserved.
+
+package "httpd"
+
+service "httpd" do
+  action [:start, :enable]
+end
+
+file "/var/www/html/index.html" do
+  content "Hello, ATO!"
+end
