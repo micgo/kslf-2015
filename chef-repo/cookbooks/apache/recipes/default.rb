24edb496f45cad45f51f27407c86cabbf57ef07e
nathenharvey <nharvey@getchef.com>
2014-10-23 07:44:07 -0400

    use a restart notification

--- chef-repo/cookbooks/apache/recipes/default.rb
+++ chef-repo/cookbooks/apache/recipes/default.rb
@@ -11,7 +11,7 @@ template "/etc/httpd/conf/httpd.conf" do
   owner  "root"
   group  "root"
   mode   "0644"
-  notifies :reload, "service[httpd]"
+  notifies :restart, "service[httpd]"
 end
 
 service "httpd" do
